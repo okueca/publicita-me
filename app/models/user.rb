fc9d2ff1a90974824ced8ae84ddaf253357a3859
pyong <muandatito@gmail.com>
2024-01-30 16:34:30 +0000

    add email and paword regx format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   devise    :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
   validates :username, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Invalid email format"}
+  validates :password, presence: true, length: { minimum: 6 }, format: {with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/, message: "Invalid password format"}
   validates :country, presence: true
   validates :role, presence: true
   enum role: [:admin, :user], validate: true 
