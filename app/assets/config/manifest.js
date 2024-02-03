3a3cf3eab0278b5e1ce0a4daf7e7c9bd03421209
pyong <muandatito@gmail.com>
2024-02-03 22:43:41 +0000

    add navbar mobile

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,9 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
-  //= link bg.mp4
+//= link bg.mp4
+//= require jquery_ujs
+//= require jquery
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
