fa6bdb5a0836934d5b072da1a07c6104257714f6
ariclinis <ariclinis1@gmail.com>
2024-02-13 18:34:38 +0000

    rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_30_005729) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_13_170517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "payments", force: :cascade do |t|
     t.string "method"
@@ -32,23 +60,31 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_005729) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "post_screens", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "screen_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_screens_on_post_id"
+    t.index ["screen_id"], name: "index_post_screens_on_screen_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "content"
     t.integer "content_type"
+    t.text "content"
   end
 
   create_table "screens", force: :cascade do |t|
     t.string "name"
     t.string "country"
     t.string "location"
-    t.integer "post_id"
     t.string "dimensions"
-    t.integer "stream_price"
+    t.float "stream_price"
     t.integer "status"
     t.string "description"
     t.datetime "created_at", null: false
@@ -78,4 +114,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_005729) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "post_screens", "posts"
+  add_foreign_key "post_screens", "screens"
 end
