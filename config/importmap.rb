9fc5361cb1a3e3010ab0ed30f8398ce05d1cf36e
ariclinis <ariclinis1@gmail.com>
2024-02-13 21:35:48 +0000

    menu bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,7 @@ pin "@rails/actiontext", to: "actiontext.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
