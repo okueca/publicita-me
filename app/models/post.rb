ebfb4d8910b8c68c605544dd57a189062e420347
ariclinis <ariclinis1@gmail.com>
2024-03-31 20:11:16 +0100

    Correction of create a screen and add validation of screen in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,16 @@
 class Post < ApplicationRecord
+    enum status: [:pending, :accepted, :posted, :done], _default: :pending
+    
     belongs_to :user
     has_one    :payment, dependent: :destroy
     has_many   :post_dates, dependent: :destroy
     has_many   :post_screens, class_name: "PostScreen", dependent: :destroy
     has_one_attached :content
+
     accepts_nested_attributes_for :post_screens, allow_destroy: true, reject_if: :all_blank 
-    enum status: [:pending, :accepted, :posted, :done], _default: :pending
+    
     validates  :title, presence: true
     validates  :content, presence: true
     validates  :user_id, presence: true
+    validates :post_screens, presence: true
 end
