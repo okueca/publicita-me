d7c0712d282034499bdac6638fbef50bdc6f06d1
pyong <muandatito@gamil.com>
2024-07-19 21:10:08 +0100

    "Update Ruby version to 3.0.7 and modify map controller to use current user location"

--- app/models/post.rb
+++ app/models/post.rb
@@ -29,6 +29,8 @@ class Post < ApplicationRecord
     validates  :user_id, presence: true
     validates :post_screens, presence: true
 
+    before_validation    :validate_attachment_file
+
     belongs_to :user
     has_one    :payment, dependent: :destroy
     has_many   :post_dates, dependent: :destroy
@@ -36,5 +38,25 @@ class Post < ApplicationRecord
     has_one_attached :content
 
     accepts_nested_attributes_for :post_screens, allow_destroy: true, reject_if: :all_blank 
+
+    private
+
+    def validate_attachment_file
+        #validate video and image
+        return unless content.attached?
+
+        #check content type and size
+        if content.attachment.blob.byte_size > 10.megabytes
+            errors.add(:content, "must be less than 10MB.")
+        end
+
+        #check content type
+        if content.attachment.blob.content_type.in?(%w(video/mp4 video/quicktime  image/jpeg image/png))
+            errors.add(:content, "must be a video or image file.") unless content.attached?
+        else
+            errors.add(:content, "must be a video or image file.")
+        end
+        
+    end
 
 end
--- app/models/post_screen.rb
+++ app/models/post_screen.rb
@@ -1,4 +1,4 @@
 class PostScreen < ApplicationRecord
-  belongs_to :post
+ # belongs_to :post
   belongs_to :screen
 end
